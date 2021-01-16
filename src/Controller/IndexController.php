e43baf4d75164191fe0d3ab8e3a5cd215854bd90
JustDylan23 <dylanvdhout@gmail.com>
2021-01-16 01:38:31 +0100

    added example templates and controller functions

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,14 +3,29 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
+/*
+ * Uncomment these 3 functions and refresh the page to see your first few pages in action
+ */
 class IndexController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    public function index(): Response
-    {
-        return $this->render('index.html.twig');
-    }
+//    #[Route('/', name: 'index')]
+//    public function index(): Response
+//    {
+//        return $this->render('pages/home/index.html.twig');
+//    }
+//
+//    #[Route('/about', name: 'about')]
+//    public function about(): Response
+//    {
+//        return $this->render('pages/about/index.html.twig');
+//    }
+//
+//    #[Route('/article/{name}', name: 'article')]
+//    public function article($name): Response
+//    {
+//        return $this->render('pages/article/index.html.twig', [
+//            'name' => $name,
+//        ]);
+//    }
 }
